fad53e77efe3991e04859e34762c07134de93ca8
Spartan-025 <caden.meints@gmail.com>
2024-09-24 09:47:56 -0500

    the 24th spet

--- Assignments/Untitled-1.py
+++ Assignments/Untitled-1.py
@@ -1,2 +1,10 @@
-age = int(input"15 ")
-print("You are", age, "years old.")
+my_number = 4           #Declaration: Creating 
+my_number = 5           #Assignment: Changing 
+print(my_number)
+
+print("type a number and I will multiply it by ten")
+num = ("")
+num = int(input(num))
+print("this is your number multiplyed by ten")
+print(num*10)
+
