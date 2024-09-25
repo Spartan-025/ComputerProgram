be7e4e09c27ccc4727e0646a1dbf9fd8a2138bc0
Spartan-025 <caden.meints@gmail.com>
2024-09-25 09:31:02 -0500

    Animal Survey with Comments

--- Assignments/animalsurvey.py
+++ Assignments/animalsurvey.py
@@ -1,13 +1,13 @@
-print("what is your favorite animal?")
-animal = input("")
-print("What is a fun fact about them?")
-funfact = input("")
-print("What do they eat")
-food = input("")
-print("What color are they?")
-color = input("")
-print("How do they move?")
-move = input("")
-print("where do they live?")
-live = input("")
-print("Your favorite animal is a " + animal + ". A fun fact about them is " + funfact + ". They eat " + food + " and they are " + color + " in color. They move by " + move + ". They live in " + live +".")
+print("what is your favorite animal?")      #asking for favorite animal 
+animal = input("")                          #Having input from the user
+print("What is a fun fact about them?")     #Asking for a fun fact 
+funfact = input("")                         #Having input from the user
+print("What do they eat")                   #Asking what they eat
+food = input("")                            #Having input from the user
+print("What color are they?")               #Asking what color they are
+color = input("")                           #Having input from the user
+print("How do they move?")                  #Asking how they move
+move = input("")                            #Having input from the user
+print("where do they live?")                #Asking where they live
+live = input("")                            #Having input from the user
+print("Your favorite animal is a " + animal + ". A fun fact about them is " + funfact + ". They eat " + food + " and they are " + color + " in color. They move by " + move + ". They live in " + live +".") #giving the users answers back to them
