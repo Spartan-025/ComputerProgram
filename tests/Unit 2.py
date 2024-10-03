b34a4ac78f08e8da48fd48f8b0922b8d0d9bd552
Spartan-025 <caden.meints@gmail.com>
2024-10-03 09:25:31 -0500

    unit_2_test

---
+++ tests/Unit 2.py
@@ -0,0 +1,28 @@
+print("Please give me 3 words.")
+word_1 = input("what is the first word\n")
+word_2 = input("what is the second word\n")
+word_3 = input("what is the third word\n")
+print("here are your words")
+print(word_1 + word_2 + word_3)
+
+
+def add_three(x, y, z):
+    print ("This is your sum")
+    the_nums = (print(x + y + z))
+x = int(input ("please give me three numbers to add\nWhat is the first number?\n"))
+y = int(input ("What is the second number you want to add?\n"))
+z = int(input ("lastly what is the third number?\n"))
+
+
+add_three (x, y, z)
+
+
+def data_three():
+    word_4 = input ("Give me a word\n")
+    integer = int(input ("Give me a whole number\n"))
+    float_1 = float(input ("Give me a number with a decmal\n"))
+    intfloat = str(integer + float_1)
+    print("Here is your final product ")
+    print (word_4 + intfloat)
+
+data_three()
